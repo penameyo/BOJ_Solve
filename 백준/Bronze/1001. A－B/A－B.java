742ab3816358f1db69c4132b30443cead9d2931f
박종호 <144550767+penameyo@users.noreply.github.com>
2024-01-21 00:38:46 +0900

    [Bronze V] Title: A-B, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());// br.readline = 한 줄을 읽는다구
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A-B);
+        
+    }
+}
