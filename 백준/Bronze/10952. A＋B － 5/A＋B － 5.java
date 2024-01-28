b8b2fb1212f8825fed6e6e36bfd468d09d20d160
JongHo Park <144550767+penameyo@users.noreply.github.com>
2024-01-28 20:37:37 +0900

    [Bronze V] Title: A+B - 5, Time: 220 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){				
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if(a == 0 && b == 0){		// 0 이 들어오면 멈추게!
+                break;
+            }
+            System.out.println(a + b);
+        }
+    }
+}
