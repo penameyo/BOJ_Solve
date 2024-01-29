214309669fca60cc1bef31e043dceb4fefb37cd9
JongHo Park <144550767+penameyo@users.noreply.github.com>
2024-01-29 23:40:45 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        if(number%n==0&&number%m==0){ // 1/29 수업내용!
+            return 1;
+        }
+        return 0;
+    }
+}
