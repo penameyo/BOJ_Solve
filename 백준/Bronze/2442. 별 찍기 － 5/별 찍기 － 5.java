aff1b5ecb0316750f1d67f94f37946cf68b0ffec
JongHo Park <144550767+penameyo@users.noreply.github.com>
2024-02-12 12:20:45 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 424 ms, Memory: 22684 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= N; i++) {
+            for (int k = N; k >i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= i; j++) {
+                System.out.print("*");
+            }
+            if(i!= 1){
+                for(int k = 0; k < i-1; k++){
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
