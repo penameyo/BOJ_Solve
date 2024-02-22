69138558f8a07abbe7504bb563e6bac88dfa93d4
JongHo Park <144550767+penameyo@users.noreply.github.com>
2024-02-22 23:03:40 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        int N = scanner.nextInt()-1;
+        scanner.close();
+
+        System.out.println(string.charAt(N));
+        
+    }
+}
